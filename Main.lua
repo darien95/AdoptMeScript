d660532c34505fd03a89e3088951301f85313c05
darien95 <darien95@gmail.com>
2024-10-30 23:10:58 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,37 +1,12 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()
-wait(90)
-
-
-
-wait(90)
-
-
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
--- Function to execute the API call
-local function executeForPlayers()
-    for _, player in ipairs(Players:GetPlayers()) do
-        local args = {
-            [1] = player -- Change this to the appropriate value as needed
-        }
-
-        -- Call the server method
-        local success, errorMessage = pcall(function()
-            ReplicatedStorage.API:FindFirstChild("HalloweenAPI/TrickOrTreatHouse"):InvokeServer(unpack(args))
-        end)
-
-        if not success then
-            warn("Error invoking API for player " .. player.Name .. ": " .. errorMessage)
-        end
-
-        -- Wait for 5 seconds before processing the next player
-        wait(5)
-    end
-end
-
--- Loop the function every 10 minutes
-while true do
-    executeForPlayers()
-    wait(300) -- Wait for 300 seconds (5 minutes)
-end
+getgenv().hub_key = "roGhh8dNr5ijH4FSeS6fNz3XdtceQPyqGCtk6hzD"
+getgenv().BabyFarmActive = true
+getgenv().AutoGraveYardBuy = true
+getgenv().trick_or_treat = true
+getgenv().TileSkipMiniGame = false
+getgenv().WhereBearMiniGame = true
+getgenv().buy_pumpkin_friend = false
+getgenv().buy_franken_feline = false
+getgenv().buy_flying_fox = false
+getgenv().buy_scarebear = false
+getgenv().buy_sea_skeleton_panda = false
+loadstring(game:HttpGet("https://nb0.xyz/scripts/2GHUB_HALLOWEEN2.lua"))()
