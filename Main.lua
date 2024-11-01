278660138ca6e626d97d012c0a85f731fefa14da
darien95 <darien95@gmail.com>
2024-11-01 08:22:15 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
-getgenv().hub_key = "API_KEY"
+getgenv().hub_key = "roGhh8dNr5ijH4FSeS6fNz3XdtceQPyqGCtk6hzD"
 getgenv().custom_label = "VPS1"
 loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
