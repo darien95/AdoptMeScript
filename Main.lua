3bc616871ceaf4351926d0a683d8be4529fcd6a2
darien95 <darien95@gmail.com>
2024-11-01 05:52:46 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,150 +1,43 @@
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2GTOOLS_EVENT_HELPER.lua"))()
-wait(999999)
+wait(5)
 local args = {
     [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = 4
+    [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = 3
+    [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = 2
+    [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = 5
+    [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = 8
+    [1] = 1
 }
-   game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
-wait(1)
 
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
 local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_franken_feline",
-    [3] = {}
+    [1] = 1
 }
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
 
-local args = {
-    [1] = "toys",
-    [2] = "halloween_2024_netzooka",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_indian_flying_fox",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_scarebear",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_headless_horse",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_marabou_stork",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_sea_skeleton_panda",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_scarebear",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_headless_horse",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_scarebear",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-local args = {
-    [1] = "pets",
-    [2] = "halloween_2024_scarebear",
-    [3] = {}
-}
-game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
-wait(1)
-
-
-
-wait(200)
-
-
-
-getgenv().hub_key = "roGhh8dNr5ijH4FSeS6fNz3XdtceQPyqGCtk6hzD"
-getgenv().BabyFarmActive = true
-getgenv().AutoGraveYardBuy = true
-getgenv().trick_or_treat = true
-getgenv().TileSkipMiniGame = false
-getgenv().WhereBearMiniGame = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-getgenv().buy_scarebear = true
-
-getgenv().buy_pumpkin_friend = true
-getgenv().buy_franken_feline = true
-getgenv().buy_flying_fox = true
-getgenv().buy_scarebear = true
-getgenv().buy_sea_skeleton_panda = true
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2GHUB_HALLOWEEN2.lua"))()
+game:GetService("ReplicatedStorage").API:FindFirstChild("HalloweenAPI/VendingMachinePurchase"):FireServer(unpack(args))
+wait(0.3)
