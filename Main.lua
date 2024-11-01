9c80f83e1210c98b7541ff22f1f40ad9451ed96e
darien95 <darien95@gmail.com>
2024-11-01 08:20:43 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,12 +1,3 @@
-getgenv().hub_key = "roGhh8dNr5ijH4FSeS6fNz3XdtceQPyqGCtk6hzD"
-getgenv().BabyFarmActive = true
-getgenv().AutoGraveYardBuy = true
-getgenv().trick_or_treat = true
-getgenv().TileSkipMiniGame = false
-getgenv().WhereBearMiniGame = true
-getgenv().buy_pumpkin_friend = false
-getgenv().buy_franken_feline = false
-getgenv().buy_flying_fox = false
-getgenv().buy_scarebear = false
-getgenv().buy_sea_skeleton_panda = false
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2GHUB_HALLOWEEN2.lua"))()
+getgenv().hub_key = "API_KEY"
+getgenv().custom_label = "VPS1"
+loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
